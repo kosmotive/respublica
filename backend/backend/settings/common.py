81928f73677f1506f5f52bd35aed7d5751f152a5
Leonid Kostrykin <void@evoid.de>
2024-01-04 20:58:47 +0100

    Add development and production settings

--- backend/backend/settings/common.py
+++ backend/backend/settings/common.py
@@ -13,19 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vg-s)f30%m&e%)=$g7_(@l47q9g2&39#@myofzsx(2op$eqoq8'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Application definition
---
+++ backend/backend/settings/development.py
@@ -0,0 +1,13 @@
+from backend.settings.common import *
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-vg-s)f30%m&e%)=$g7_(@l47q9g2&39#@myofzsx(2op$eqoq8'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
